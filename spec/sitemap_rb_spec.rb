177773b0719907706413a0e90c06ee04085bdd8b
Adam Gamble <adamgamble@gmail.com>
2020-09-06 21:25:45 -0500

    Added urls support

--- spec/sitemap_rb_spec.rb
+++ spec/sitemap_rb_spec.rb
@@ -19,4 +19,20 @@ RSpec.describe SitemapRb do
       expect(@results.keys).to eq([:sitemaps])
     end
   end
+  describe "urls" do
+    let(:sitemap_path) { File.join('spec', 'support', 'url_sitemap.xml')}
+    let(:sitemap) { SitemapRb::Sitemap.new }
+
+    before do
+      @results = sitemap.load_from_file(sitemap_path)
+    end
+
+    it "should grab the sitemap rows" do
+      expect(@results[:urls].count).to eq(4)
+    end
+
+    it "should contain urls and last modified" do
+      expect(@results[:urls].first).to eq({change_frequency: 'daily', url: 'https://www.vecteezy.com/vector-art/131429-sweet-16-watercolor-background', last_modified: '2020-09-07T00:01:56+00:00', images: [{:title=>"Sweet 16 Watercolor Background", :url=>"https://static.vecteezy.com/system/resources/previews/000/131/429/original/vector-sweet-16-watercolor-background.jpg"}, {:title=>"Sweet 16 Watercolor Background", :url=>"https://static.vecteezy.com/system/resources/previews/000/131/429/non_2x/vector-sweet-16-watercolor-background.jpg"}, {:title=>"Sweet 16 Watercolor Background", :url=>"https://static.vecteezy.com/system/resources/previews/000/131/429/large_2x/vector-sweet-16-watercolor-background.jpg"}, {:title=>"Sweet 16 Watercolor Background", :url=>"https://static.vecteezy.com/system/resources/thumbnails/000/131/429/original/sweet-16-watercolor-background.jpg"}]})
+    end
+  end
 end
